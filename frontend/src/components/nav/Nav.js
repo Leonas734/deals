4d76cf0dc8292281b21fedf5c82a81f0bc3dbef4
Leonas734 <leonasadomaitis@gmail.com>
2022-02-07 10:43:11 +0000

    Refactored nav.
    Nav now has a mobile search bar. When user clicks search icon a mobile friendly search bar appears.

--- frontend/src/components/nav/Nav.js
+++ frontend/src/components/nav/Nav.js
@@ -5,6 +5,7 @@ import styles from "./Nav.module.css";
 import searchIcon from "../../assets/search-icon.svg";
 import plusIcon from "../../assets/plus-icon.svg";
 import userIcon from "../../assets/user-icon.svg";
+import closeIcon from "../../assets/x-icon.svg";
 
 import LoginModal from "../modals/LoginModal";
 import RegisterModal from "../modals/RegisterModal";
@@ -12,6 +13,7 @@ import NavAccount from "./NavAccount";
 import { useAuth } from "../context/authContext";
 
 function Nav() {
+  const [showMobileSearchBar, setShowMobileSearchBar] = useState(false);
   const [mobileView, setMobileView] = useState(false);
   const [showLoginModal, setShowLoginModal] = useState(false);
   const [showRegisterModal, setShowRegisterModal] = useState(false);
@@ -28,6 +30,7 @@ function Nav() {
         setMobileView(true);
       } else {
         setMobileView(false);
+        setShowMobileSearchBar(false);
       }
     });
   }, [userAuthState]);
@@ -51,41 +54,56 @@ function Nav() {
           modalIsOpen={showRegisterModal}
         />
       )}
-      <h1 className={styles["nav-title"]} onClick={() => navigate("/")}>
-        Deals
-      </h1>
-      {!mobileView && (
-        <div className={styles["nav-search-bar"]}>
+      {!showMobileSearchBar && (
+        <h1 className={styles["nav-title"]} onClick={() => navigate("/")}>
+          Deals
+        </h1>
+      )}
+      {(!mobileView || showMobileSearchBar) && (
+        <div
+          className={`${styles["nav-search-bar"]} ${
+            showMobileSearchBar ? styles["nav-search-bar-mobile"] : null
+          }`}>
           <img
             className={styles["nav-search-bar-icon"]}
             src={searchIcon}
             alt="Search"
           />
+          {showMobileSearchBar && (
+            <img
+              src={closeIcon}
+              alt="Close"
+              className={`${styles["nav-search-bar-close-icon"]} ${styles["nav-search-bar-icon"]}`}
+              onClick={() => setShowMobileSearchBar(!showMobileSearchBar)}
+            />
+          )}
           <input className={styles["nav-search-bar-input"]} />
         </div>
       )}
-      <div className={styles["nav-buttons"]}>
-        {mobileView && (
-          <div className={styles["nav-button"]}>
-            <img src={searchIcon} className={styles["nav-icon"]} alt="Search" />
-          </div>
-        )}
-        {!userAuthState && (
-          <div
-            className={`${styles["nav-button"]}`}
-            data-cy="nav-login-register-button"
-            onClick={() => setShowLoginModal(true)}>
-            <img src={userIcon} className={styles["nav-icon"]} alt="Person" />
-            <p>Login / Register</p>
-          </div>
-        )}
-        {userAuthState && (
-          <NavAccount user={userAuthState} logout={logUserOut} />
-        )}
-        <div className={`${styles["nav-button"]} `}>
-          <img src={plusIcon} className={styles["nav-icon"]} alt="Plus" />
-          <p>Submit</p>
+      {mobileView && !showMobileSearchBar && (
+        <div
+          className={`${styles["nav-button"]} ${styles["nav-button-search"]}`}
+          onClick={() => setShowMobileSearchBar(!showMobileSearchBar)}>
+          <img
+            src={searchIcon}
+            className={`${styles["nav-icon"]}`}
+            alt="Search"
+          />
         </div>
+      )}
+      {!userAuthState && (
+        <div
+          className={`${styles["nav-button"]} ${styles["nav-button-login-register"]}`}
+          data-cy="nav-login-register-button"
+          onClick={() => setShowLoginModal(true)}>
+          <img src={userIcon} className={styles["nav-icon"]} alt="Person" />
+          <p>Login / Register</p>
+        </div>
+      )}
+      {userAuthState && <NavAccount user={userAuthState} logout={logUserOut} />}
+      <div className={`${styles["nav-button"]} ${styles["nav-button-submit"]}`}>
+        <img src={plusIcon} className={styles["nav-icon"]} alt="Plus" />
+        <p>Submit</p>
       </div>
     </div>
   );
