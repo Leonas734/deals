0d00af8955de85bc7bac41be44edccd04f16d061
Leonas734 <leonasadomaitis@gmail.com>
2022-02-17 11:30:04 +0000

    Added cy.wait inside user_view - Test waits for API call to hit before continueing the testing.

---
+++ frontend/cypress/integration/user_view.spec.js
@@ -0,0 +1,32 @@
+describe("User view", function () {
+  it("Can view user with all details", function () {
+    const userName = "test";
+    const dateJoined = new Date();
+    const profilePicture = "default_image.jpg";
+    const totalDealsPosted = 3;
+    cy.intercept("GET", `/api/user/${userName}`, {
+      body: {
+        username: userName,
+        profile_picture: profilePicture,
+        total_deals_posted: totalDealsPosted,
+        date_joined: dateJoined,
+      },
+    }).as("userDetails");
+
+    cy.visit(`user/${userName}`);
+    cy.wait("@userDetails");
+    cy.get("p").contains(userName).should("have.text", userName);
+    cy.get("p")
+      .contains("Total deals posted:")
+      .should("have.text", `Total deals posted: ${totalDealsPosted}`);
+    cy.get("p")
+      .contains("Member since:")
+      .should("have.text", `Member since: ${dateJoined.toDateString("en-GB")}`);
+    cy.get("p")
+      .contains("Member since:")
+      .should("have.text", `Member since: ${dateJoined.toDateString("en-GB")}`);
+    cy.get("[data-cy=user-view-profile-picture]")
+      .should("have.attr", "src")
+      .should("include", profilePicture);
+  });
+});
